450439d88c441573743f6550b29c9f4cf6bde7d1
taksapoy <ploytaksaporn2648@gmail.com>
2023-12-13 12:11:53 +0700

    zxzcz

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using API.Controllers;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Controller;
+
+#nullable disable
+
+public class ErrorController : BaseApiController
+{
+    private readonly DataContext _dataContext;
+    public ErrorController(DataContext dataContext) 
+    {
+        _dataContext = dataContext;
+    }
+
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetSecret()
+    {
+        return "xxx";
+    }
+
+      [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound ()
+    {
+        var user = _dataContext.Users.Find(-1) ;
+        if (user is null) return NotFound() ;
+        return user ;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError ()
+    {
+        
+        {
+            var user = _dataContext.Users.Find(-1) ;
+            var stringUser = user.ToString() ; //can not turn null to string = no reference exception
+            return stringUser ;
+        }
+        
+    }
+
+     [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("illegal request");
+    }
+
+}
